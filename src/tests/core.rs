9e15f27198ac1c52dd7581663874171b80a9982f
lmeller-git <louis.meller@icloud.com>
2026-08-20 12:03:10 +0200

    add some tests

--- src/tests/core.rs
+++ src/tests/core.rs
@@ -16,62 +16,260 @@ use crate::{
     },
 };
 
-#[test]
-fn smoke_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    smoke(q.buy_in());
+mod dcbo {
+    use super::*;
+    #[test]
+    fn smoke_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        smoke(q.buy_in());
+    }
+
+    #[test]
+    fn smoke_long_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        smoke_long(q.buy_in());
+    }
+
+    #[test]
+    fn force_push_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        force_push(q.buy_in());
+    }
+
+    #[test]
+    fn len_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        len(q.buy_in());
+    }
+
+    #[test]
+    fn len_empty_full_impl() {
+        let q: InlineBandit<LockedDeque<()>, DCBO, 2, 1> = InlineBandit::new();
+        len_empty_full(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        mpmc(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_ring_buffer_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        mpmc_ring_buffer(q.buy_in());
+    }
+
+    #[test]
+    fn mpsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        mpsc(q.buy_in());
+    }
+
+    #[test]
+    fn spsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
+        spsc(q.buy_in());
+    }
+
+    #[test]
+    fn linearizable_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DoubleCollect<DCBO>, 2> = InlineBandit::new();
+        linearizable(q.buy_in());
+    }
 }
 
-#[test]
-fn smoke_long_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    smoke_long(q.buy_in());
+mod dra {
+    use super::*;
+    use crate::strategy::DRA;
+
+    #[test]
+    fn smoke_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        smoke(q.buy_in());
+    }
+
+    #[test]
+    fn smoke_long_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        smoke_long(q.buy_in());
+    }
+
+    #[test]
+    fn force_push_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        force_push(q.buy_in());
+    }
+
+    #[test]
+    fn len_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        len(q.buy_in());
+    }
+
+    #[test]
+    fn len_empty_full_impl() {
+        let q: InlineBandit<LockedDeque<()>, DRA, 2, 1> = InlineBandit::new();
+        len_empty_full(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        mpmc(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_ring_buffer_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        mpmc_ring_buffer(q.buy_in());
+    }
+
+    #[test]
+    fn mpsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        mpsc(q.buy_in());
+    }
+
+    #[test]
+    fn spsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DRA, 2> = InlineBandit::new();
+        spsc(q.buy_in());
+    }
+
+    #[test]
+    fn linearizable_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DoubleCollect<DRA>, 2> = InlineBandit::new();
+        linearizable(q.buy_in());
+    }
 }
 
-#[test]
-fn force_push_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    force_push(q.buy_in());
+mod random {
+    use super::*;
+    use crate::strategy::RandomAccess;
+
+    #[test]
+    fn smoke_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        smoke(q.buy_in());
+    }
+
+    #[test]
+    fn smoke_long_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        smoke_long(q.buy_in());
+    }
+
+    #[test]
+    fn force_push_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        force_push(q.buy_in());
+    }
+
+    #[test]
+    fn len_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        len(q.buy_in());
+    }
+
+    #[test]
+    fn len_empty_full_impl() {
+        let q: InlineBandit<LockedDeque<()>, RandomAccess, 2, 1> = InlineBandit::new();
+        len_empty_full(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        mpmc(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_ring_buffer_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        mpmc_ring_buffer(q.buy_in());
+    }
+
+    #[test]
+    fn mpsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        mpsc(q.buy_in());
+    }
+
+    #[test]
+    fn spsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RandomAccess, 2> = InlineBandit::new();
+        spsc(q.buy_in());
+    }
+
+    #[test]
+    fn linearizable_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DoubleCollect<RandomAccess>, 2> = InlineBandit::new();
+        linearizable(q.buy_in());
+    }
 }
 
-#[test]
-fn len_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    len(q.buy_in());
-}
+mod round_robin {
+    use super::*;
+    use crate::strategy::RoundRobin;
 
-#[test]
-fn len_empty_full_impl() {
-    let q: InlineBandit<LockedDeque<()>, DCBO, 2, 1> = InlineBandit::new();
-    len_empty_full(q.buy_in());
-}
+    #[test]
+    fn smoke_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        smoke(q.buy_in());
+    }
 
-#[test]
-fn mpmc_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    mpmc(q.buy_in());
-}
+    #[test]
+    fn smoke_long_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        smoke_long(q.buy_in());
+    }
 
-#[test]
-fn mpmc_ring_buffer_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    mpmc_ring_buffer(q.buy_in());
-}
+    #[test]
+    fn force_push_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        force_push(q.buy_in());
+    }
 
-#[test]
-fn mpsc_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    mpsc(q.buy_in());
-}
+    #[test]
+    fn len_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        len(q.buy_in());
+    }
 
-#[test]
-fn spsc_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DCBO, 2> = InlineBandit::new();
-    spsc(q.buy_in());
-}
+    #[test]
+    fn len_empty_full_impl() {
+        let q: InlineBandit<LockedDeque<()>, RoundRobin, 2, 1> = InlineBandit::new();
+        len_empty_full(q.buy_in());
+    }
 
-#[test]
-fn linearizable_impl() {
-    let q: InlineBandit<LockedDeque<u32>, DoubleCollect<DCBO>, 2> = InlineBandit::new();
-    linearizable(q.buy_in());
+    #[test]
+    fn mpmc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        mpmc(q.buy_in());
+    }
+
+    #[test]
+    fn mpmc_ring_buffer_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        mpmc_ring_buffer(q.buy_in());
+    }
+
+    #[test]
+    fn mpsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        mpsc(q.buy_in());
+    }
+
+    #[test]
+    fn spsc_impl() {
+        let q: InlineBandit<LockedDeque<u32>, RoundRobin, 2> = InlineBandit::new();
+        spsc(q.buy_in());
+    }
+
+    #[test]
+    fn linearizable_impl() {
+        let q: InlineBandit<LockedDeque<u32>, DoubleCollect<RoundRobin>, 2> = InlineBandit::new();
+        linearizable(q.buy_in());
+    }
 }
